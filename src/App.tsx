f6a24114644582efe0d71aeb5ee70fa6642376b6
Raphael Alexei <raphaeeelbr@gmail.com>
2023-12-16 17:23:43 -0300

    Refactor
    fix: Melhorei a estrutura do código e utilizei Operadores em ENUM para melhor leitura.

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,132 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const calculate = (operator: string, a: number, b: number): number => {
+enum Operator {
+  Addition = "+",
+  Subtraction = "-",
+  Multiplication = "*",
+  Division = "/",
+}
+
+const calculate = (operator: Operator, a: number, b: number): number => {
   switch (operator) {
-    case "+":
+    case Operator.Addition:
       return a + b;
-    case "-":
+    case Operator.Subtraction:
       return a - b;
-    case "*":
+    case Operator.Multiplication:
       return a * b;
-    default:
+    case Operator.Division:
       return a / b;
-  }
-};
-
-const priority = (operator: string): number => {
-  switch (operator) {
-    case "(":
-      return 0;
-    case "+":
-    case "-":
-      return 1;
     default:
-      return 2;
+      throw new Error("Invalid operator");
   }
 };
 
-const App = () => {
-  const [expression, setExpression] = useState<string>("");
-  const [currentExpression, setCurrentExpression] = useState("");
+const getPriority = (operator: Operator): number => {
+  switch (operator) {
+    case Operator.Addition:
+    case Operator.Subtraction:
+      return 1;
+    case Operator.Multiplication:
+    case Operator.Division:
+      return 2;
+    default:
+      return 0;
+  }
+};
+
+const App: React.FC = () => {
+  const [currentExpression, setCurrentExpression] = useState<string>("");
   const [history, setHistory] = useState<string[]>([]);
 
-  const digit = (value: string): void => {
-    setExpression((prevExpression) => prevExpression + value);
+  const handleInput = (value: string): void => {
+    setCurrentExpression((prevExpression) => prevExpression + value);
   };
 
   const clearDisplay = (): void => {
-    setExpression("");
+    setCurrentExpression("");
   };
 
-  const resolve = (expression: string) => {
+  const resolveExpression = (expression: string): string => {
     const numbers: number[] = [];
-    const operators: string[] = [];
+    const operators: Operator[] = [];
+
+    let currentNumber = '';
     for (let i = 0; i < expression.length; i++) {
       const char = expression[i];
       if (char === " ") continue;
 
-      const num = Number(char);
-      if (!isNaN(num)) {
-        numbers.push(num);
+      const isNumeric = !isNaN(parseInt(char)) || char === '.';
+      if (isNumeric) {
+        currentNumber += char;
+        continue;
+      }
+      
+      if (currentNumber) {
+        numbers.push(parseFloat(currentNumber));
+        currentNumber = '';
         continue;
       }
 
       if (char === "(") {
-        operators.push(char);
+        operators.push(Operator.Addition);
         continue;
       }
-
-      if (["+", "-", "*", "/"].includes(char)) {
-        if (operators.length == 0) {
-          operators.push(char);
-          continue;
-        }
-
-        while (operators.length > 0 && priority(char) <= priority(operators[operators.length - 1])) {
-          const num1 = numbers.pop()!;
+      
+      if (Object.values(Operator).includes(char as Operator)) {
+        while (operators.length > 0 && getPriority(char as Operator) <= getPriority(operators[operators.length - 1])) {
           const num2 = numbers.pop()!;
-          const operator = operators.pop()!;
-          numbers.push(calculate(operator, num1, num2));
+          const num1 = numbers.pop()!;
+          const op = operators.pop()!;
+          numbers.push(calculate(op, num1, num2));
         }
+        operators.push(char as Operator);
+        continue;
       }
-
-      if(char === ")"){
-        while(operators[operators.length - 1] !== "("){
-          const num1 = numbers.pop()!;
+      
+      if (char === ")") {
+        while (operators[operators.length - 1] !== Operator.Addition) {
           const num2 = numbers.pop()!;
-          const operator = operators.pop()!;
-          numbers.push(calculate(operator, num1, num2));
+          const num1 = numbers.pop()!;
+          const op = operators.pop()!;
+          numbers.push(calculate(op, num1, num2));
         }
         operators.pop();
+        continue;
       }
-
-      while (operators.length > 0) {
-        const num1 = numbers.pop()!;
-        const num2 = numbers.pop()!;
-        const operator = operators.pop()!;
-        numbers.push(calculate(operator, num1, num2));
-      }
-
-      const result = numbers.pop();
-      return result !== undefined ? result.toString() : "Error";
     }
+
+    while (operators.length > 0) {
+      const num2 = numbers.pop()!;
+      const num1 = numbers.pop()!;
+      const op = operators.pop()!;
+      numbers.push(calculate(op, num1, num2));
+    }
+
+    const result = numbers.pop();
+    return result !== undefined && !isNaN(result) ? result.toString() : "Error";
   };
 
   const handleCalculate = () => {
-    const result = resolve(expression);
-    setCurrentExpression(result);
-    setHistory([...history, `${expression} = ${result}`]);
-  }
+    if (currentExpression === "") return;
+    const result = resolveExpression(currentExpression);
+    setHistory([...history, `${currentExpression} = ${result}`]);
+    clearDisplay();
+  };
 
   return (
     <>
       <div className="calculator">
-        <input type="text" value={expression} className="display" />
+        <input
+          type="text"
+          value={currentExpression}
+          onChange={(e) => setCurrentExpression(e.target.value)}
+          className="display"
+        />
         <div className="buttons">
-          {[...Array(10).keys(), "+", "-", "*", "/", ".", "(", ")"].map((key) => (
-            <button key={key} onClick={() => digit(key.toString())}>
+          {[...Array(10).keys(), Operator.Addition, Operator.Subtraction, Operator.Multiplication, Operator.Division, ".", "(", ")"].map((key) => (
+            <button key={key} onClick={() => handleInput(key.toString())}>
               {key}
             </button>
           ))}
@@ -117,7 +138,16 @@ const App = () => {
           </button>
         </div>
       </div>
+      <div className="history">
+        <h2>Histórico</h2>
+        <ul>
+          {history.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
+
 export default App;
